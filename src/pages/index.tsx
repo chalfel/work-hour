5bf1fc083fdc2606dc334e6bf808e90d647803c3
Caio Felix <caiohalcsik@gmail.com>
2021-12-19 16:24:58 -0300

    feat/done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,97 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
+import React, { useState } from 'react'
+type InputProp = {
+  label: string,
+  value: string,
+  id: string,
+  onChange: (value: string, id: string) => void
+}
+const Input: React.FC<InputProp> = ({ label, onChange, value, id }) => {
+  return (
+    <>
+      <label className="text-neutral-500">{label} </label>
+      <input className="text-center" type="time" value={value} onChange={(e) => onChange(e.target.value, id)}></input>
+    </>
+  )
+}
 
 const Home: NextPage = () => {
-  const [ enterTime, setEnterTime ] = useState('00:00')
-  const [ lunchTime, setLunchTime ] = useState('00:00')
-  const [ goBackTime, setGoBackTime ] = useState('00:00')
-  const [ leaveTime, setLeaveTime ] = useState('00:00')
-  const [ totalTime, setTotalTime ] = useState('00:00')
+  const [totalTime, setTotalTime] = useState('00:00')
+  const [timeState, setTimeState] = useState([
+    {
+      label: 'Hora da Entrada',
+      value: '00:00',
+      id: '0'
+    },
+    {
+      label: 'Hora do Almoço',
+      value: '00:00',
+      id: '1'
+    },
+    {
+      label: 'Hora da Volta',
+      value: '00:00',
+      id: '2'
+    },
+    {
+      label: 'Hora da Saída',
+      value: '00:00',
+      id: '3'
+    },
+  ])
+
 
   const handleCalculateTime = () => {
-    if (enterTime && lunchTime && goBackTime && leaveTime) {
-      const enter = parseInt(enterTime.split(':')[0]) * 60 + parseInt(enterTime.split(':')[1])
-      const lunch = parseInt(lunchTime.split(':')[0]) * 60 + parseInt(lunchTime.split(':')[1])
-      const goBack = parseInt(goBackTime.split(':')[0]) * 60 + parseInt(goBackTime.split(':')[1])
-      const leave = parseInt(leaveTime.split(':')[0]) * 60 + parseInt(leaveTime.split(':')[1])
-      const total = leave - enter - lunch - goBack
-      const totalMinutes = Math.abs(total % 60)
-      setTotalTime(`${24 + Math.floor(total / 60)}:${totalMinutes < 10 ? `0${totalMinutes}` : totalMinutes}`)
-    }
+    const test = timeState.reduce((acc, curr) => {
+      return [...acc, curr.value]
+    }, [] as string[])
+      .map(arg => parseInt(arg.split(':')[0]) * 60 + parseInt(arg.split(':')[1]))
+    const total2 = (test[1] - test[0]) + (test[3] - test[2])
+    const totalMinutes = Math.floor(total2 % 60)
+    setTotalTime(`${Math.floor(total2 / 60)}:${totalMinutes < 10 ? `0${totalMinutes}` : totalMinutes}`)
   }
-  return (
-    <main className="w-screen h-screen flex flex-col items-center justify-center">
-      <p className="font-black text-6xl lg:text-8xl">Work Hour</p>
-      <form className="flex flex-col p-5 items-center justify-start">
-        <p className="text-2xl text-neutral-500 font-normal">Resultado </p>
-        <p className="text-3xl font-bold mb-4"> {totalTime} </p>
-        <label className="text-neutral-500">Hora da Entrada</label>
-        <input className="text-center" type="time" value={enterTime} onChange={(e) => setEnterTime(e.target.value)}></input>
-        <label className="text-neutral-500" htmlFor="lunch">Hora do Almoço</label>
-        <input className="text-center" id="lunch" type="time" value={lunchTime} onChange={(e) => setLunchTime(e.target.value)}></input>
-        <label className="text-neutral-500" htmlFor="goBack">Hora da Volta</label>
-        <input className="text-center" id="goBack" type="time" value={goBackTime} onChange={(e) => setGoBackTime(e.target.value)}></input>
-        <label className="text-neutral-500"htmlFor="leave">Hora da Saida</label>
-        <input  className="text-center" id="leave" type="time" value={leaveTime} onChange={(e) => setLeaveTime(e.target.value)}></input>
-        <button onClick={handleCalculateTime} type="button" className="bg-black text-white px-4 py-2 rounded-xl my-2">Calcular</button>
-      </form>
-    </main>
-  )
+  const handleAddField = () => {
+    setTimeState((prev) => [...prev, 
+    {
+      label: 'Entrada Opcional',
+      value: '00:00',
+      id: `${prev.length}`
+    },
+    {
+      label: 'Saida Opcional',
+      value: '00:00',
+      id: `${prev.length + 1}`
+    }
+  ]
+    )
+  }
+
+  const handleOnStateChange = (value: string, id: string) => {
+    const newArray = timeState.map(item => {
+      if(id === item.id) {
+        return {
+          ...item,
+          value,
+        }
+      }
+      return item
+    })      
+
+      setTimeState(newArray)
+}
+return (
+  <main className="w-screen h-screen flex flex-col items-center justify-center">
+    <p className="font-black text-6xl lg:text-8xl">Work Hour</p>
+    <form className="flex flex-col p-5 items-center justify-start">
+      <p className="text-2xl text-neutral-500 font-normal">Resultado </p>
+      <p className="text-3xl font-bold mb-4"> {totalTime} </p>
+      {timeState.map((time) => (<Input key={time.id} value={time.value} label={time.label} id={time.id} onChange={handleOnStateChange} />))}
+      <button onClick={handleCalculateTime} type="button" className="bg-black text-white px-4 py-2 rounded-xl my-2">Calcular</button>
+      <button onClick={handleAddField} type="button" className="bg-black text-white px-4 py-2 rounded-xl my-2">Adicionar Campo</button>
+
+    </form>
+  </main>
+)
 }
 
 export default Home
